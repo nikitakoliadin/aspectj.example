8d08843c2f4011fa2289ec8f4269bc35c1850519
Колядин Никита Александрович <nikita.koljadin@privatbank.ua>
2019-08-30 15:28:47 +0300

    Updated Application class: - Removed logs;

--- src/main/java/com/qthegamep/aspectj/example/Application.java
+++ src/main/java/com/qthegamep/aspectj/example/Application.java
@@ -2,17 +2,11 @@ package com.qthegamep.aspectj.example;
 
 import com.qthegamep.aspectj.example.service.TestService;
 import com.qthegamep.aspectj.example.service.TestServiceImpl;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 public class Application {
 
-    private static final Logger LOG = LoggerFactory.getLogger(Application.class);
-
     public static void main(String[] args) {
-        LOG.info("<<<<<<------------START------------>>>>>>");
         TestService testService = new TestServiceImpl();
         testService.testMethod();
-        LOG.info("<<<<<<------------END------------>>>>>>");
     }
 }
